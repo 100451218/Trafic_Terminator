dbc104849f4385940e744a15d0ac64b5f72ea4d9
100451218 <100451218@alumnos.uc3m.es>
2022-04-29 18:47:30 +0200

    Inicio del dictionary.

--- data_loader/load_try.py
+++ data_loader/load_try.py
@@ -1,6 +1,30 @@
-import pandas as pd
+import csv
+file = open(r'C:\Users\l022c418\Downloads\Data.csv')
+csvreader = csv.reader(file)
+
+Super={"HHH":{"N":0,"W":0,"E":0},
+"HHL":{"N":0,"W":0,"E":0},
+"HLH":{"N":0,"W":0,"E":0},
+"HLL":{"N":0,"W":0,"E":0},
+"LHH":{"N":0,"W":0,"E":0},
+"LHL":{"N":0,"W":0,"E":0},
+"LLH":{"N":0,"W":0,"E":0}
+}
 
 
-df = pd.read_excel (r'C:\Users\l022c418\Downloads\Data.xlsx')
 
-print (df)
+
+
+
+for i in csvreader:
+    get_elements = i[0].split(";")
+    if get_elements[0][0]=="I":
+        pass
+    else:
+        i_state=get_elements[0][0]+get_elements[1][0]+get_elements[2][0]
+        action=get_elements[3]
+        f_state=get_elements[4][0]+get_elements[5][0]+get_elements[6][0]
+        print(i_state, action, f_state)
+        Super[i_state][action]=Super[i_state][action]+1
+
+print(Super)
